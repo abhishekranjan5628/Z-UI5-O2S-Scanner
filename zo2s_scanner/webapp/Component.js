699b7b003b25158b8d594d708a40e8a18ff70b90
abhishek ranjan <abhishekranjan5628@gmail.com>
2024-10-29 09:52:17 +0000

    Initial UI of application

---
+++ zo2s_scanner/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zo2sscanner.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
